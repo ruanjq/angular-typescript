a5692ac5816fad198c79792c9eaa75188dab2853
DESKTOP-2MPH7MB\ruanj <1016744743@qq.com>
2017-04-08 15:20:12 +0800

    获取IP 测试

--- web-server.js
+++ web-server.js
@@ -42,6 +42,28 @@ HttpServer.prototype.start = function (port) {
     this.port = port;
     this.server.listen(port);
     util.puts('Http Server running at http://localhost:' + port + '/');
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function (ifname) {
+      var alias = 0;
+
+      ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+          // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+          return;
+        }
+
+        if (alias >= 1) {
+          // this single interface has multiple ipv4 addresses
+          console.log(ifname + ':' + alias, iface.address);
+        } else {
+          // this interface has only one ipv4 adress
+          console.log(ifname, iface.address);
+        }
+        ++alias;
+      });
+    });
 };
 
 HttpServer.prototype.parseUrl_ = function (urlString) {
